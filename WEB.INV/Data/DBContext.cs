b63c1ac898167690fe8910df5e46123cec500d03
RELIC7663 <alexispepinosa28@gmail.com>
2025-02-02 11:11:53 -0500

    DEV

--- WEB.INV/Data/DBContext.cs
+++ WEB.INV/Data/DBContext.cs
@@ -4,20 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using NTI.INV;
+ 
+using WEB.INT;
 
-    public class DBContext : IdentityDbContext
+public class DBContext : IdentityDbContext
     {
         public DBContext (DbContextOptions<DBContext> options)
             : base(options)
         {
         }
 
-        public DbSet<NTI.INV.Permisos> Permisos { get; set; } = default!;
+        public DbSet<WEB.INT.Permisos> Permisos { get; set; } = default!;
 
-        public DbSet<NTI.INV.Usuarios> Usuarios { get; set; } = default!;
+        public DbSet<WEB.INT.Usuarios> Usuarios { get; set; } = default!;
 
-        public DbSet<NTI.INV.Productos> Productos { get; set; } = default!;
+        public DbSet<WEB.INT.Productos> Productos { get; set; } = default!;
 
-        public DbSet<NTI.INV.Categoria> Categoria { get; set; } = default!;
+        public DbSet<WEB.INT.Categoria> Categoria { get; set; } = default!;
     }
